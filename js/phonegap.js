51dfed67f1d4558eec3484c48df918b73a5dc9e9
Zamtem <zamtem@mail.ru>
2014-06-19 14:53:26 +0400

    Update phonegap.js

--- js/phonegap.js
+++ js/phonegap.js
@@ -6945,3 +6945,18 @@ require('cordova/channel').onNativeReady.fire();
 
 
 })();var PhoneGap = cordova;
+
+document.addEventListener("deviceready", onDeviceReady, false);
+function onDeviceReady() {
+    document.addEventListener("backbutton", function (e) {
+        e.preventDefault(); 
+    navigator.notification.confirm("Are you sure want to exit from App?", onConfirmExit, "Confirmation", "Yes,No");
+    }, false );
+}
+function onConfirmExit(button) {
+if(button==2){ //If User select a No, then return back;
+    return;
+}else{
+    navigator.app.exitApp(); // If user select a Yes, quit from the app.
+}
+}
